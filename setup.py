a85b13bd0d6345d3adbc062c11addbe0ba810cfe
hussain_syrer <h2311065@gmail.com>
2026-05-14 16:25:59 +0200

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="agency-cli",
+    name="agency-agents",
     version="0.1.0",
     author="Agency Team",
     author_email="contact@agency-agents.com",
