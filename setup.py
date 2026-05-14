a99b634d41aa1cb78dcfde0f35d84e505783a77e
hussain_syrer <h2311065@gmail.com>
2026-05-14 16:30:14 +0200

    update

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="agency-agents",
+    name="agency-intelligence",
     version="0.1.0",
-    author="Agency Team",
-    author_email="contact@agency-agents.com",
+    author="Hussain Alkhatib",
+    author_email="h2311065@gmail.com",
     description="Professional AI Agent Orchestration CLI",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/agency-cli",
+    url="https://github.com/HussainAlkatib/agency-agents",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
